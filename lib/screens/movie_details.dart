90f3d015917cd55be0721c26ecf3e6d596067361
Mishan20 <ishansenanayaka1@gmail.com>
2024-10-17 22:19:02 +0530

    Created movie details model and company details model

---
+++ lib/screens/movie_details.dart
@@ -0,0 +1,221 @@
+import 'package:flutter/material.dart';
+import 'package:tmdb_movie_app/models/movie_details_model.dart';
+import 'package:tmdb_movie_app/models/movies_model.dart';
+import 'package:tmdb_movie_app/services/api_services.dart';
+
+class MovieDetails extends StatefulWidget {
+  final Movie movie;
+  const MovieDetails({required this.movie, super.key});
+
+  @override
+  State<MovieDetails> createState() => _MovieDetailsState();
+}
+
+class _MovieDetailsState extends State<MovieDetails> {
+  ApiService service = ApiService();
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.only(top: 30),
+      child: Scaffold(
+        backgroundColor:
+            Colors.grey[900], // Dark background for a cinematic feel
+        body: FutureBuilder(
+          future: service.getDetails(id: widget.movie.id.toString()),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              MovieDetailsModel data = snapshot.data!;
+              return SafeArea(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      // Movie banner with gradient effect
+                      Stack(
+                        children: [
+                          Container(
+                            width: double.infinity,
+                            height: size.height * 0.3,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                    "https://image.tmdb.org/t/p/w500${widget.movie.backdropPath}"),
+                                fit: BoxFit.cover,
+                                colorFilter: ColorFilter.mode(
+                                    // ignore: deprecated_member_use
+                                    Colors.black.withOpacity(0.4),
+                                    BlendMode.darken),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            height: size.height * 0.3,
+                            decoration: const BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [Colors.transparent, Colors.black],
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            bottom: 20,
+                            left: 20,
+                            child: Text(
+                              widget.movie.title.toString(),
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      // Movie Poster and Tagline
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: size.width * 0.4,
+                              height: size.height * 0.3,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                boxShadow: [
+                                  BoxShadow(
+                                    // ignore: deprecated_member_use
+                                    color: Colors.black.withOpacity(0.7),
+                                    blurRadius: 10,
+                                    offset: const Offset(0, 5),
+                                  ),
+                                ],
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                    "https://image.tmdb.org/t/p/w500${widget.movie.posterPath}",
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 20),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    data.tagline ?? '',
+                                    style: const TextStyle(
+                                      color: Colors.white70,
+                                      fontStyle: FontStyle.italic,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      // Movie Overview
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          widget.movie.overview.toString(),
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            height: 1.5,
+                          ),
+                          textAlign: TextAlign.justify,
+                        ),
+                      ),
+
+                      // Production Companies
+                      if (data.company!.isNotEmpty)
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                "Production Companies",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              SizedBox(
+                                height: 120,
+                                child: ListView.builder(
+                                  scrollDirection: Axis.horizontal,
+                                  itemCount: data.company!.length,
+                                  itemBuilder: (context, index) {
+                                    final company = data.company![index];
+                                    return Padding(
+                                      padding: const EdgeInsets.only(right: 16),
+                                      child: Column(
+                                        children: [
+                                          company.logo.toString() != ""
+                                              ? Container(
+                                                  width: 100,
+                                                  height: 100,
+                                                  decoration: BoxDecoration(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            10),
+                                                    image: DecorationImage(
+                                                      image: NetworkImage(
+                                                        "https://image.tmdb.org/t/p/w500${company.logo}",
+                                                      ),
+                                                      fit: BoxFit.cover,
+                                                    ),
+                                                    boxShadow: [
+                                                      BoxShadow(
+                                                        color: Colors.black
+                                                            // ignore: deprecated_member_use
+                                                            .withOpacity(0.7),
+                                                        blurRadius: 8,
+                                                      )
+                                                    ],
+                                                  ),
+                                                )
+                                              : const SizedBox(),
+                                          Text(
+                                            company.name.toString(),
+                                            style: const TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 14,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+              );
+            }
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.amber,
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:tmdb_movie_app/models/movies_model.dart';
 
+import '../models/movie_details_model.dart';
+
 class ApiService {
   final apiKey = "api_key=9edcdf5f2e36f8892a5a96e9e4976f7f";
   final baseUrl = "https://api.themoviedb.org/3/movie/popular?";
@@ -21,6 +23,18 @@ class ApiService {
       return movies;
     } else {
       throw Exception(response.statusCode.toString());
+    }
+  }
+
+
+Future<MovieDetailsModel> getDetails({required String id}) async {
+    Response response =
+        await get(Uri.parse("https://api.themoviedb.org/3/movie/$id?$apiKey"));
+    if (response.statusCode == 200) {
+      Map<String, dynamic> json = jsonDecode(response.body);
+      return MovieDetailsModel.fromJson(json);
+    } else {
+      throw Exception(response.statusCode);
     }
   }
 }
